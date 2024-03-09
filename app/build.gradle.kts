8eaabeafda545cf93dd35318d24cfe5421aa9072
Max Intellij Laptop <ms4623@ic.ac.uk>
2024-03-09 17:12:33 +0000

    New HTTP Retrofit dependancies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(files("retrofit-2.9.0.jar"))
+    implementation(files("converter-gson-2.9.0.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/cropmate/weather/WeatherGetter.kt
+++ app/src/main/java/com/example/cropmate/weather/WeatherGetter.kt
@@ -1,31 +1,33 @@
 package com.example.cropmate.weather
 
-class WeatherGetter {
-/*    fun fetchWeatherData(cityName: String, apiKey: String) {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://api.openweathermap.org/data/2.5/")
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+
+class WeatherGetter : AppCompatActivity() {
+    private val API_KEY = "42cc4d60bd8c91b9ff79e2057a5f5f6a"
+    private lateinit var weatherService: WeatherService
+
+    fun getWeather(): Weather {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(WeatherService::class.java)
 
-        val weatherApi = retrofit.create(OpenWeatherMapApi::class.java)
 
-        weatherApi.getCurrentWeatherData(cityName, apiKey).enqueue(object : Callback<WeatherResponse> {
-            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
-                response.body()?.let {
-                    Log.d("WeatherData", "Date: ${Date()}")
-                    Log.d("WeatherData", "Temperature: ${it.main.temp}")
-                    Log.d("WeatherData", "Cloud Cover: ${it.clouds.all}")
-                    Log.d("WeatherData", "Humidity: ${it.main.humidity}")
-                    // Rainfall data if available
-                    it.rain?.let { rain ->
-                        Log.d("WeatherData", "Rainfall: ${rain.last3hours}")
-                    }
-                }
-            }
+        return TODO()
+    }
 
-            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                Log.e("WeatherData", "Error fetching weather data", t)
-            }
-        })
+
+    /*private fun updateUI(weatherData: WeatherData) {
+        findViewById<TextView>(R.id.textViewCity).text = weatherData.name
+        findViewById<TextView>(R.id.textViewTemperature).text =
+            "${weatherData.main.temp.toInt()}°C"
+        val iconUrl = "https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png"
+        Glide.with(this)
+            .load(iconUrl)
+            .into(findViewById(R.id.imageViewWeatherIcon))
     }*/
 }
