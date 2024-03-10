d3b42fb13196f07dc86929bb1cfc4ebafbae088f
VeerV11 <veervishalvij@gmail.com>
2024-03-10 00:32:47 +0000

    skrrt

---
+++ app/src/main/java/com/example/cropmate/ProduceItem.kt
@@ -0,0 +1,7 @@
+package com.example.cropmate
+
+class ProduceItem(override val Id: String, override val Name: String, override val Quantity: String, override val unit: String): Item {
+    override fun toString(): String {
+        return "$Name : $Quantity $unit"
+    }
+}
